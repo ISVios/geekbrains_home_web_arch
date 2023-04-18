018744451f9218760bedb24ffeac231b7cff304c
vios <i.svoboda.vios@gmail.com>
2023-04-18 15:10:19 +0300

    update render func

--- framework/utils/render.py
+++ framework/utils/render.py
@@ -1,12 +1,8 @@
-from jinja2 import Template
-from os.path import join
+from jinja2 import FileSystemLoader, Environment
 
 
 def render(template_name, folder="templates", **kwgs):
-    """
-    Todo add docstring
-    """
-    file_path = join(folder, template_name)
-    with open(file_path, encoding="utf-8") as f:
-        template = Template(f.read())
+    env = Environment()
+    env.loader = FileSystemLoader(folder)
+    template = env.get_template(template_name)
     return template.render(**kwgs)
