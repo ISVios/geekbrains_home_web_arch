15ab957fc02774ef16f2bffe421f1b5d7abe066e
vios <i.svoboda.vios@gmail.com>
2023-05-17 21:54:59 +0300

    need_auth decor

--- framework/utils/decorators.py
+++ framework/utils/decorators.py
@@ -3,6 +3,29 @@ from framework import FrameWork
 from framework.views.view import FuncView
 
 
+def need_auth(
+    redirect_to_url: str | None = None, redirect_to_namespace: str | None = None
+):
+    def _func(func):
+        def wrap(self, view_env, config, result, **kwds):
+            if not view_env.user.is_auth():
+                if redirect_to_url:
+                    result.redirect_to_url(redirect_to_url)
+                elif redirect_to_namespace:
+                    result.redirect_to_namespace(redirect_to_namespace)
+                else:
+                    raise NotImplementedError
+                    pass  # Think
+                return None
+            func_res = func(self, view_env, config, result, **kwds)
+
+            return func_res
+
+        return wrap
+
+    return _func
+
+
 def to_url(url: str, namespace: str):
     def _func(func):
         framework = FrameWork()
